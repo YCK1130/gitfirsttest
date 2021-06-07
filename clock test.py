d5859f3d3716549f06bad1d7f0a194427eb32835
Ken Yang <ken1130yang@gmail.com>
2021-06-07 22:21:39 +0800

    clock.py add some annotation

--- clock test.py
+++ clock test.py
@@ -2,15 +2,16 @@ from tkinter import *
 from time import *
 
 def update():
-    timestr = strftime("%I:%M:%S %p")
+    timestr = strftime("%I:%M:%S %p") #I for hour,M for minute,S for secand ,p for a.m. or p.m.
     time_label.config(text=timestr)
 
-    window.after(100,update)
+    window.after(100,update)  #(sleep time (ms),function)
 
 window = Tk()
 window.config(bg="black")
 time_label = Label(window,font=("Arial",30),fg="#00FF00",bg="black")
 time_label.place(x=0,y=0)
 time_label.pack()
+
 update()
 window.mainloop()
